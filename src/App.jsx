0bbd398321a52e22e4e749d3a31a935027c53d1e
Khawla192 <202005171@stu.uob.edu.bh>
2025-02-04 18:58:17 +0300

    Index for all the pets

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import * as petService from './services/petService';
+import PetList from './components/PetList/PetList.jsx';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [pets, setPets] = useState([]);
+
+  useEffect (() => {
+    const fetchPets = async () => {
+      const fetchedPets = await petService.index();
+      if (fetchedPets.error) {
+        throw new Error(fetchedPets.error)
+      }
+      setPets(fetchedPets);
+    };
+    fetchPets();
+  }, []);
+
+  return (
+    <>
+      <PetList pets={pets}/>
+    </>
+  )
 };
 
 export default App;
